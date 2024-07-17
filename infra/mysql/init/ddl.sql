272147afeb3bf9657d4e477ae0dd7964189185bf
tusmasoma <s.k18@icloud.com>
2024-07-17 15:04:55 +0900

    chore: add ddl and dml

---
+++ infra/mysql/init/ddl.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS `gamedb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `gamedb` ;
+
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Collections CASCADE;
+DROP TABLE IF EXISTS Scores CASCADE;
+DROP TABLE IF EXISTS User_Collections CASCADE;
+
+-- Users Table
+CREATE TABLE Users (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    coins INT DEFAULT 0,
+    high_score INT DEFAULT 0
+);
+
+-- Collections Table
+CREATE TABLE Collections (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    rarity INT NOT NULL,
+    weight INT NOT NULL
+);
+
+-- Scores Table
+CREATE TABLE Scores (
+    id CHAR(36) PRIMARY KEY,
+    user_id CHAR(36),
+    value INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+-- UserCollections Table
+CREATE TABLE User_Collections (
+    id CHAR(36) PRIMARY KEY,
+    user_id CHAR(36),
+    collection_id CHAR(36),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (collection_id) REFERENCES Collections(id)
+);
