812398a1599fac066c2e5d015fa76041a8c6c0d4
tusmasoma <s.k18@icloud.com>
2024-07-17 16:30:38 +0900

    feat: add serve

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"github.com/tusmasoma/go-tech-dojo/pkg/log"
+)
+
+const (
+	ReadTimeout                   = 5 * time.Second
+	WriteTimeout                  = 10 * time.Second
+	IdleTimeout                   = 15 * time.Second
+	GracefulShutdownTimeout       = 5 * time.Second
+	PreflightCacheDurationSeconds = 300
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var addr string
+	// .envファイルから環境変数を読み込む
+	if err := godotenv.Load(); err != nil {
+		log.Info("No .env file found", log.Ferror(err))
+	}
+	flag.StringVar(&addr, "addr", ":8083", "tcp host:port to connect")
+	flag.Parse()
+
+	Serve(addr)
+}
+
+func Serve(addr string) {
+	/* ===== URLマッピングを行う ===== */
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Origin"},
+		ExposedHeaders:   []string{"Link", "Authorization"},
+		AllowCredentials: false,
+		MaxAge:           PreflightCacheDurationSeconds,
+	}))
+
+	/* ===== サーバの設定 ===== */
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      r,
+		ReadTimeout:  ReadTimeout,
+		WriteTimeout: WriteTimeout,
+		IdleTimeout:  IdleTimeout,
+	}
+	/* ===== サーバの起動 ===== */
+	log.Info("Server running...")
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt, os.Kill)
+	defer stop()
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("Server failed", log.Ferror(err))
+		}
+	}()
+
+	<-ctx.Done()
+	log.Info("Server stopping...")
+
+	tctx, cancel := context.WithTimeout(context.Background(), GracefulShutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(tctx); err != nil {
+		log.Error("Failed to shutdown http server", log.Ferror(err))
+	}
+	log.Info("Server exited")
 }
